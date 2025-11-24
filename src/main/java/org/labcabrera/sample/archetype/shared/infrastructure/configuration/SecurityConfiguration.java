695a841c02d9ae566a9df77efa3956b508d3dc02
labcabrera <lab.cabrera@gmail.com>
2025-11-24 23:45:47 +0100

    Updated contract-first example.

--- src/main/java/org/labcabrera/sample/archetype/shared/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/org/labcabrera/sample/archetype/shared/infrastructure/configuration/SecurityConfiguration.java
@@ -35,7 +35,7 @@ public class SecurityConfiguration {
                     "/swagger-ui.html",
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
-                    "/openapi/api.yaml",
+                    "/api-definition.yaml",
                     "/actuator/health",
                     "/actuator/info")
                 .permitAll()
